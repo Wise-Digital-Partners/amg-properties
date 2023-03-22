2644d69061dbc40d153bb29a6058878f021980be
Zac Wood <57919069+Zac-Wood@users.noreply.github.com>
2023-03-22 13:39:18 -0700

    Update review-us.js

--- web/src/pages/review-us.js
+++ web/src/pages/review-us.js
@@ -17,13 +17,7 @@ const Page = ({ data }) => {
       <section className="pt-16 md:pt-20 pb-22 md:pb-30 bg-gray-50">
         <div className="container">
           <div className="max-w-3xl mx-auto">
-            <iframe
-              src="https://amg-props-reviews-platform.flywheelsites.com/"
-              title="Reviews Platform"
-              width="100%"
-              height="1500px"
-              border="0"
-            />
+           
           </div>
         </div>
       </section>
